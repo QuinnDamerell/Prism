20a0a84b438ae8a2bf687f73d68f1d934b83672e
Quinn Damerell <qdamere@microsoft.com>
2019-05-19 09:31:19 -0700

    Changing the way the background process runs.

--- PrismPi/main.cpp
+++ PrismPi/main.cpp
@@ -13,59 +13,13 @@
 
 int main(int argc, char **argv)
 {
-    bool isBackground = false;
-    if(argc > 1)
-    {
-        // Indicate we are running in the background.
-        isBackground = true;
-        
-        // If we have an arguemnt launch as a daemon        
-        pid_t pid, sid;
-        
-        std::cout << "Launching Prism as a background process\n";
-        
-        // Fork off of the parent process
-        pid = fork();
-        if (pid < 0) 
-        {
-            std::cout << "Failed to launch!\n";       
-            exit(EXIT_FAILURE);
-        }
-
-        // If success kill the parent process
-        if (pid > 0) 
-        {
-            std::cout << "Successfully launched!\n";            
-            exit(EXIT_SUCCESS);
-        }
-
-        // Change the file mode mask
-        umask(0);
-                
-        // Create a new SID for the child process
-        sid = setsid();
-        if (sid < 0) 
-        {
-            exit(EXIT_FAILURE);
-        }
-        
-        // Change the current working directory
-        if ((chdir("/")) < 0) 
-        {
-            exit(EXIT_FAILURE);
-        }
-        
-        /* Close out the standard file descriptors */
-        close(STDIN_FILENO);
-        close(STDOUT_FILENO);
-        close(STDERR_FILENO);
-    }   
+    bool dontTakeInput = argc > 1;
     
     // Create the host
     PrismCommandHostPtr prism = std::make_shared<PrismCommandHost>();
 
     // This will block forever
-    prism->Run(!isBackground);
+    prism->Run(!dontTakeInput);
 
     // This should never return.
     return 0;
